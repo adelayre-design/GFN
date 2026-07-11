b667dcec09de4ac78ff707a70362910fabc0306a
Simon Almero <saalmero@gbox.adnu.edu.ph>
2026-07-11 22:50:48 +0800

    fix back end error

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,39 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const frontendDistPath = path.resolve(__dirname, "..", "dist");
+const frontendIndexPath = path.join(frontendDistPath, "index.html");
+
+if (fs.existsSync(frontendDistPath)) {
+  app.use(express.static(frontendDistPath));
+}
+
 app.get("/api/health", (req, res) => {
   res.json({ message: "API is running" });
+});
+
+app.get("/", (req, res) => {
+  if (fs.existsSync(frontendIndexPath)) {
+    return res.sendFile(frontendIndexPath);
+  }
+
+  res.status(200).send("GymFit backend is running");
+});
+
+app.use((req, res) => {
+  if (fs.existsSync(frontendIndexPath)) {
+    return res.sendFile(frontendIndexPath);
+  }
+
+  res.status(404).send("Cannot GET " + req.originalUrl);
 });
 
 const PORT = process.env.PORT || 5000;
